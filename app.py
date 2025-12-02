7004aca25b92b60222213ed7fea29f0d3dbae139
Lucassigma123 <lucass126@nycstudents.net>
2025-12-02 10:16:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import requests
 
-def getrickymort(rickymort):
- response = requests.get(f"https://rickandmortyapi.com{rickymort()}")
- if response.status_code != 200:
-     print("Error fetching data!")
-     return None
- data = response.json()
- return {
+def getrickymort(name):
+  response = requests.get(f"https://rickandmortyapi.com/api/character/?name={name.lower()}")
+  if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+  data = response.json()
+  return {
         "name": data["name"],
- 
+        "status": data["status"],
+        "species": data["species"],
+        "type": data["type"],
+        "gender": data["gender"],
+        "origin": data["origin"]
+        
+      }
+CHARACTER=getrickymort("Morty")
+print(CHARACTER)
