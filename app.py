8107f1a4b2ffb191de24cf2338a5ac5df14da98f
Lucassigma123 <lucass126@nycstudents.net>
2025-12-09 20:28:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import random
 def getrickymort(name):
   response = requests.get(f"https://rickandmortyapi.com/api/character/?name={name.lower()}")
   if response.status_code != 200:
@@ -26,10 +27,12 @@ def search_CHARACTER():
       outcome_label.config(text=str(CHARACTER))
       print(CHARACTER)
 def randomfact():
-     x="Rick Is Afraid Of Pirates"
-     y="Rick And Birdperson Share A Kiss In The Comics"
-     z="Mr. Poopybutthole Was The Parasite Catalyst"
-     print(x)
+    facts=["Rick Is Afraid Of Pirates",
+           "Rick And Birdperson Share A Kiss In The Comics",
+           "Mr. Poopybutthole Was The Parasite Catalyst"
+    ]
+    print(random.choice(facts))
+   
 Label=tk.Label(root,text="USE THE SEARCH BAR TO SEARCH FOR RICK AND MORTY CHARACTERS")
 Label.pack(pady=10)
 search =tk.Entry(root,width=50)
@@ -37,6 +40,6 @@ search.pack(pady=10)
 tk.Button(root,text="search",command=search_CHARACTER ).pack(pady=10)
 outcome_label= tk.Label(root,font=("Georgia",12),text="",)# outcome of the search 
 outcome_label.pack(pady=50)
-tk.Button(root,text="random fact",command=)
+tk.Button(root,text="random fact",command=randomfact).pack(pady=200)
 root.mainloop()
 
