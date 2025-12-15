c63aed8912c06897d18be9ffb6a7af1b042f2c44
Lucassigma123 <lucass126@nycstudents.net>
2025-12-15 09:53:05 -0500

    Update .py

--- .py
+++ .py
@@ -20,6 +20,9 @@ def getrickymort(name):
       }
 import tkinter as tk
 root = tk.Tk()
+Home = tk.Frame(root)
+quiz = tk.Frame(root)
+Home.pack(fill="both", expand=True)
 root.title("RICK AND MORTY  CHARACTERS")
 root.geometry("1280x800")
 
@@ -33,21 +36,24 @@ def randomfact():
     y=(random.choice(facts))
     outcomefact.config(text=str(y))
 
-Label=tk.Label(root,text="USE THE SEARCH BAR TO SEARCH FOR RICK AND MORTY CHARACTERS")
+Label=tk.Label(Home,text="USE THE SEARCH BAR TO SEARCH FOR RICK AND MORTY CHARACTERS")
 Label.pack(pady=10)
-search =tk.Entry(root,width=50)
+search =tk.Entry(Home,width=50)
 search.pack(pady=10)
 
-tk.Button(root,text="SEARCH",command=search_CHARACTER ).pack(pady=10)
-outcome_label= tk.Label(root,font=("Georgia",12),text="",)# outcome of the search                                                                                                                                                                                                                                                                                                                                 
+tk.Button(Home,text="SEARCH",command=search_CHARACTER ).pack(pady=10)
+outcome_label= tk.Label(Home,font=("Georgia",12),text="",)# outcome of the search                                                                                                                                                                                                                                                                                                                                 
 outcome_label.pack(pady=50)
 
-tk.Button(root,text="RANDOM FACT",command=randomfact).pack(pady=30)
-outcomefact=tk.Label(root,font=("Georgia",12),text="",)
+tk.Button(Home,text="RANDOM FACT",command=randomfact).pack(pady=30)
+outcomefact=tk.Label(Home,font=("Georgia",12),text="",)
 outcomefact.pack(pady=40)
 
+def show_quiz():
+    Home.pack_forget()
+    quiz.pack(fill="both", expand=True)
+     
 
-def questions():
-    tk.Button(root,text="Quiz",command=).pack(pady=50)
+tk.Button(Home,text="Quiz",command=show_quiz).pack(pady=50)
 
 root.mainloop()
